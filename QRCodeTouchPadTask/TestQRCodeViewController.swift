961e8f831aef76ceeba54455fd816c4afa8785a9
kailashinventyv2024 <kailash.rajput.inventyv@gmail.com>
2025-03-20 13:09:22 +0530

    added QRCode scan and touch pad sign

---
+++ QRCodeTouchPadTask/TestQRCodeViewController.swift
@@ -0,0 +1,29 @@
+//
+//  TestQRCodeViewController.swift
+//  QRCodeTouchPadTask
+//
+//  Created by Kailash Rajput on 20/03/25.
+//
+
+import UIKit
+
+class TestQRCodeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
